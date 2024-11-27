734706be48e98001a57e6c627e7e38191d12b373
Cassiano26 <candidocassiano2@gmail.com>
2024-11-27 12:22:21 -0300

    style(tailwindConfig): Add default primary colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'primary': {
+          90: '#1A0524',
+          80: '#3C0B54',
+          70: '#5E1185',
+          60: '#8017B5',
+          50: '#AA1EF1',
+          40: '#AA1EF1D9', 
+          30: '#AA1EF1A6',
+          20: '#AA1EF173',
+          10: '#AA1EF140', 
+        }
       },
     },
   },
